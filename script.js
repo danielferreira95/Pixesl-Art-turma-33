04b21d050c88295329deadb400d1d17a52155ad3
DanielFerreira <daniel18mtm@gmail.com>
2023-05-22 15:03:31 -0300

    Requisito 2 e 3 feitos

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const lista = document.querySelectorAll('.color');
 const botao = document.querySelector('#button-random-color');
-
-const corLista = () => {
-    lista[0].style.background = 'black';
-    lista[1].style.background = gerar_cor();
-    lista[2].style.background = gerar_cor();
-    lista[3].style.background = gerar_cor();
-}
-corLista();
+const elementoPai = document.querySelector('#pixel-board')
+const selected = document.querySelector('.selected');
+const palete = document.querySelector('#color-palette')
 
 function gerar_cor() {
 
@@ -21,4 +16,51 @@ function gerar_cor() {
  
  }
 
- botao.addEventListener('click', corLista());
+ const corLista = () => {
+    const cor0 = lista[0].style.background = 'black';
+    const cor1 = lista[1].style.background = gerar_cor();
+    const cor2 = lista[2].style.background = gerar_cor();
+    const cor3 = lista[3].style.background = gerar_cor();
+    const array = [cor0, cor1, cor2, cor3];
+    localStorage.setItem('colorPalette', JSON.stringify(array));
+}
+
+
+ botao.addEventListener('click', () => corLista());
+
+ const nova = () => {
+    const guarda = JSON.parse(localStorage.getItem('colorPalette'));
+    for(let index = 0; index < lista.length; index+=1){
+        lista[index].style.background = guarda[index];
+    }
+ };
+
+ nova();
+
+ const quadro = () => {
+    elementoPai.style.width = '210px';
+    for(let index = 0; index<5; index+=1){
+        for(let index = 0; index<5; index+=1){
+            const elemento = document.createElement('div');
+            elemento.className = 'pixel';
+            elemento.style.background = 'white';
+            elemento.style.border = 'solid black 1px';
+            elemento.style.height = '40px';
+            elemento.style.width = '40px';
+            elemento.style.display = 'inline-block';
+            elementoPai.appendChild(elemento);
+        }
+    }
+ }
+ quadro();
+
+ const muda = () => {
+    palete.addEventListener('click', (event) => {
+        for(let index = 0; index<lista.length; index+=1){
+            lista[index].classList.remove('selected');
+        }
+        event.target.classList.add('selected');
+    })
+ };
+ muda();
+ 
