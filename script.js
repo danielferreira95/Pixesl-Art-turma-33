d5be2304a59d88e5c4c304f3264460411c0a5771
DanielFerreira <daniel18mtm@gmail.com>
2023-06-14 12:49:58 -0300

    Do requisito 1 ao 11

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const lista = document.querySelectorAll('.color');
 const botao = document.querySelector('#button-random-color');
-const elementoPai = document.querySelector('#pixel-board')
+const board = document.querySelector('#pixel-board')
 const selected = document.querySelector('.selected');
 const palete = document.querySelector('#color-palette')
+const limpaQuadro = document.querySelector('#clear-board')
+const cor0 = lista[0].style.background = 'rgb(0, 0, 0)';
+const cor1 = lista[1].style.background = 'red';
+const cor2 = lista[2].style.background = 'green';
+const cor3 = lista[3].style.background = 'blue';
 
-function gerar_cor() {
+//Requisito 2 e 3
+function gerarCor() {
 
     let r = parseInt(Math.random() * 255);
  
@@ -12,55 +18,110 @@ function gerar_cor() {
  
     let b = parseInt(Math.random() * 255);
  
-    return `rgba(${r}, ${g}, ${b})`;
+    return `rgb(${r}, ${g}, ${b})`;
  
  }
 
+ // Requisito 5 e 8
  const corLista = () => {
-    const cor0 = lista[0].style.background = 'black';
-    const cor1 = lista[1].style.background = gerar_cor();
-    const cor2 = lista[2].style.background = gerar_cor();
-    const cor3 = lista[3].style.background = gerar_cor();
+    const cor0 = lista[0].style.background = 'rgb(0, 0, 0)';
+    const cor1 = lista[1].style.background = gerarCor();
+    const cor2 = lista[2].style.background = gerarCor();
+    const cor3 = lista[3].style.background = gerarCor();
     const array = [cor0, cor1, cor2, cor3];
     localStorage.setItem('colorPalette', JSON.stringify(array));
 }
 
+// Requisito 4
+botao.addEventListener('click', corLista);
 
- botao.addEventListener('click', () => corLista());
 
- const nova = () => {
-    const guarda = JSON.parse(localStorage.getItem('colorPalette'));
-    for(let index = 0; index < lista.length; index+=1){
-        lista[index].style.background = guarda[index];
-    }
- };
 
- nova();
-
+// Requisito 6 e 7
  const quadro = () => {
-    elementoPai.style.width = '210px';
+    board.style.width = '230px';
     for(let index = 0; index<5; index+=1){
         for(let index = 0; index<5; index+=1){
-            const elemento = document.createElement('div');
-            elemento.className = 'pixel';
-            elemento.style.background = 'white';
-            elemento.style.border = 'solid black 1px';
-            elemento.style.height = '40px';
-            elemento.style.width = '40px';
-            elemento.style.display = 'inline-block';
-            elementoPai.appendChild(elemento);
+            const quadrinho = document.createElement('div');
+            quadrinho.classList.add('pixel');
+            quadrinho.style.background = 'white';
+            quadrinho.style.border = 'solid 1px black';
+            quadrinho.style.height = '40px';
+            quadrinho.style.width = '40px';
+            quadrinho.style.display = 'inline-block';
+            quadrinho.style.margin = '2px'; 
+            quadrinho.style.verticalAlign = 'middle';
+            quadrinho.style.borderRadius = '10%';
+            board.appendChild(quadrinho);
         }
     }
  }
  quadro();
 
- const muda = () => {
+ //Requisito 9
+ const recarrega = () => {
+    const pegaCor = JSON.parse(localStorage.getItem('colorPalette'));
+    if(pegaCor){
+      for(let index = 0; index<lista.length; index += 1){
+          lista[index].style.backgroundColor = pegaCor[index];
+      }
+    }
+};
+recarrega();
+
+
+// Requisito 8 e 10
+
+ const clica = () => {
+
+    board.addEventListener('click', (event) => {
+        const quadrinhoSelecionado = event.target;
+        quadrinhoSelecionado.style.backgroundColor = 'black'; 
+    });
     palete.addEventListener('click', (event) => {
-        for(let index = 0; index<lista.length; index+=1){
-            lista[index].classList.remove('selected');
-        }
-        event.target.classList.add('selected');
+        const alvoDoEvento = event.target;
+        const cor = alvoDoEvento.style.backgroundColor;
+        lista.forEach((paletinha) => {
+            if(paletinha.classList.contains('selected')){
+            paletinha.classList.remove('selected');
+        }});
+        alvoDoEvento.classList.add('selected');
+        board.addEventListener('click', (event) => {
+            const quadrinhoSelecionado = event.target;
+            quadrinhoSelecionado.style.backgroundColor = cor; 
+        })
     })
  };
- muda();
+ clica();
+
+
+ //Requisito 11
+
+ const quadrinho = document.querySelectorAll('.pixel');
+
+ const limpaBoard = () => {
+        limpaQuadro.addEventListener('click', () => {
+            for(let index = 0; index < quadrinho.length; index += 1){
+                quadrinho[index].style.backgroundColor = 'rgb(255, 255, 255)';
+                console.log('limpa');
+            }
+        })
+ };
+limpaBoard();
+
+
+
+//Requisito 12
+
+// const guardaQuadro = () => {
+//     const quadradinho = document.querySelectorAll('.pixel');
+//     const corQuadradinho = quadradinho.style.backgroundColor;
+//     localStorage.setItem('pixelBoard', JSON.stringify(corQuadradinho));
+//     // if(localStorage.getItem('pixelBoard')){
+
+//     // }
+// };
+// guardaQuadro();
+
+
  
